15211f9300da8d46e030fc634b3a00760da476a8
christopher <cjwright4242@gmail.com>
2017-06-16 16:46:33 -0400

    ENH: add build status

--- pack_utils/utils.py
+++ pack_utils/utils.py
@@ -10,16 +10,21 @@ def main(user, password):
     d = [(z['name'], z['organization']['html_url']) for z in b if z['name'] != 'all-members']
     d.sort()
 
-    template = '|[{}]({}/{}-feedstock) | [![Anaconda-Server Badge]' \
+    template = '|[{name}]({org}/{name}-feedstock) | [![Anaconda-Server Badge]' \
                '(https://anaconda.org/conda-forge' \
-               '/{}/badges/downloads.svg)]' \
-               '(https://anaconda.org/conda-forge/{}) |\n'
+               '/{name}/badges/downloads.svg)]' \
+               '(https://anaconda.org/conda-forge/{name}) | {linux} | {osx} | {windows} |\n'
+    linux = '[![Circle CI](https://circleci.com/gh/conda-forge/{name}-feedstock.svg?style=shield)](https://circleci.com/gh/conda-forge/{name}-feedstock)'
+    osx = '[![TravisCI](https://travis-ci.org/conda-forge/{name}-feedstock.svg?branch=master)](https://travis-ci.org/conda-forge/{name}-feedstock)'
+    windows = '[![AppVeyor](https://ci.appveyor.com/api/projects/status/github/conda-forge/{name}-feedstock?svg=True)](https://ci.appveyor.com/project/conda-forge/{name}-feedstock/branch/master)'
 
     with open('../readme.md', 'w') as f:
         f.write('## Packages I help to maintain\n')
-        f.write('| Package | Downloads |\n')
-        f.write('|:---------:|:--------:|\n')
-        f.writelines([template.format(i, j, i, i, i) for i, j in d])
+        f.write('| Package | Downloads | Linux | OSX | Windows |\n')
+        f.write('|:---------:|:--------:|:---------:|:--------:|:---------:|\n')
+        l = [template.format(linux=linux, osx=osx, windows=windows, name=i,
+                             org=j).format(name=i, org=j) for i, j in d]
+        f.writelines(l)
         f.write('\n\n')
         f.write("""Feel free to use this script, you just need to provide your GitHub username, 
 and password/token.
